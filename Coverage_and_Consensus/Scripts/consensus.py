42ad2f8dc77a948bd9d1ab96d896ab094c4e54c6
Goncalves <victorgoncalves@college.harvard.edu>
2022-02-26 10:00:28 -0500

    consensus.py coding

--- Coverage_and_Consensus/Scripts/consensus.py
+++ Coverage_and_Consensus/Scripts/consensus.py
@@ -1,16 +1,33 @@
+from mimetypes import init
 import numpy as np
 import matplotlib.pyplot as plt
+
+add_noise = 1
+
+# Graph theory concepts!
+# degree == number of neighbors
+# path, connectivity
+# adjacency list
+
+# A graph is connected if all verticies are connected 
+
 
 # nodes for storing information
 class Node(object):
 
     def __init__(self, init_state):
 
+        # adds the gaussian noise w st. d = 0.1
+        if add_noise:
+            init_state += np.random.normal(scale = 0.1)
+            
         self._prev_state = init_state
         self._next_state = init_state
 
     # store the state update
     def update(self, update):
+        if add_noise:
+            update += np.random.normal(scale = 0.1)
         self._next_state += update
 
     # push the state update
@@ -55,9 +72,8 @@ class Graph(object):
         self.node_list = node_list
         self.adj_matrix = adj_matrix
 
-
         self._epsilon = epsilon
-
+        
         self._finished = False      # bool determining when we've reached a threshold
         self._threshold = threshold
         self._sigma = sigma
@@ -67,18 +83,28 @@ class Graph(object):
 
         # Returns a list of nodes that are neighbors to the input node
         def neighbors(node):
+            neighbor_list = []
             for i in range(len(self.node_list)):
-                    print()
-            return []
-
-        a = self.adj_matrix
-        epsilon = self._epsilon
+                if self.adj_matrix[node.id][i]:
+                    neighbor_list.append(self.node_list[i])
+            return neighbor_list
 
 
-        # Want to update each node in node list
-        for node in self.node_list:
-            value = 0
+        adj = self.adj_matrix
+        
+        for i in range(len(adj)):
+            curr_node = self.node_list[i]
+            sum = 0
+
+            for j in range(len(adj[i])):
+                curr_neighbor = self.node_list[j]
+
+                sum += adj[i][j] * (curr_neighbor.state - curr_node.state)
             
+            curr_node.update(self._epsilon * sum)
+
+        for node in self.node_list:
+            node.step()
 
     # return the state of the nodes currently - you can disable print here
     def node_states(self):
@@ -87,18 +113,21 @@ class Graph(object):
         for node in self.node_list:
             string = string + node.state.astype('str') + "\t"
             out.append(node.state)
-        # print(string)
 
         return out
 
     # check if the graph has reached consensus somehow, even if there are adversaries
     def is_finished(self):
+        consensus_val = self.node_list[0].state
+        for node in self.node_list:
+            if node.state != consensus_val:
+                return False
+        return True
         
-        raise NotImplementedError
-
     @property
     def finished(self):
-        # add your code here
+        self._finished = self.is_finished()
+
         return self._finished
 
 
@@ -131,35 +160,41 @@ if __name__ == "__main__":
 
     node_list = [Node(4.0), Node(2.0), Node(-1.0), Node(3.0), Node(0.0)]
 
+    # stoch_altered_node_list = [Node(np.random.normal(scale = 0.1) + node.state()) for node in node_list]
+
     # linear formation
-    adj_matrix = np.array([[0, 1, 0, 0, 0],
+    linear = np.array([[0, 1, 0, 0, 0],
                             [1, 0, 1, 0, 0],
                             [0, 1, 0, 1, 0],
                             [0, 0, 1, 0, 1],
                             [0, 0, 0, 1, 0]])
 
     # circular formation
-    # adj_matrix = np.array([[0, 1, 0, 0, 1],
-    #                         [1, 0, 1, 0, 0],
-    #                         [0, 1, 0, 1, 0],
-    #                         [0, 0, 1, 0, 1],
-    #                         [1, 0, 0, 1, 0]])
+    circular = np.array([[0, 1, 0, 0, 1],
+                            [1, 0, 1, 0, 0],
+                            [0, 1, 0, 1, 0],
+                            [0, 0, 1, 0, 1],
+                            [1, 0, 0, 1, 0]])
 
-    # fully connected
-    # adj_matrix = np.array([[0, 1, 1, 1, 1],
-    #                         [1, 0, 1, 1, 1],
-    #                         [1, 1, 0, 1, 1],
-    #                         [1, 1, 1, 0, 1],
-    #                         [1, 1, 1, 1, 0]])
+    # fully connected formation
+    fully_conected = np.array([[0, 1, 1, 1, 1],
+                            [1, 0, 1, 1, 1],
+                            [1, 1, 0, 1, 1],
+                            [1, 1, 1, 0, 1],
+                            [1, 1, 1, 1, 0]])
 
-    graph = Graph(node_list, adj_matrix)
-    node_states = []
-    for _ in range(10):
-        graph.update_graph()
-        node_states.append(graph.node_states())
+    matrix_list = [linear, circular, fully_conected]
 
-    node_states = np.array(node_states)
-    plot_states(node_states)
+    for adj_matrix in matrix_list:
+        graph = Graph(node_list, adj_matrix)
+        node_states = []
+        for _ in range(100):
+        # while not graph.finished:
+            graph.update_graph()
+            node_states.append(graph.node_states())
+
+        node_states = np.array(node_states)
+        plot_states(node_states)
     
 
 
