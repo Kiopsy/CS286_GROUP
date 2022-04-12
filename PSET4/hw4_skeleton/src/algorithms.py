2a375afe2aa1fd7f36afd8b1a1a4069fb28ac31b
Dominic Garrity <dgarrity@college.harvard.edu>
2022-04-12 15:32:33 -0700

    Changed number of simulations to 50 and re-ran.

--- PSET4/hw4_skeleton/src/algorithms.py
+++ PSET4/hw4_skeleton/src/algorithms.py
@@ -14,20 +14,21 @@ class standard_rollout:
         ################################# Begin your code ###############################
         # Use itertools to compute cartesian product of control component sets
         controls = itertools.product(*list_of_all_control_component_sets)
-        return controls
+        return list(controls)
         ################################# End your code ###############################
 
 
     def expectation_for_minimization(self, taxi_state_object, control):
         expected_cost = 0
-        for num_simu in range(10):
+        simulations = 50
+        for num_simu in range(simulations):
             state_object = copy.deepcopy(taxi_state_object) #use state_object for further operations inside this function
             # update expected_cost using the 1) the next_state with the given control 2) future cost using the average_MC_simulation using base policy
             ################################# Begin your code ###############################
             # Add stage cost and expected future cost to expected_cost
             expected_cost += state_object.next_state(control) + average_MC_simulation(state_object)
             ################################# End your code ###############################
-        expected_cost /= 10.0
+        expected_cost /= float(simulations)
         return expected_cost
 
     def minimization_of_expectations(self, control_cost):
@@ -37,7 +38,7 @@ class standard_rollout:
         # This funtion should return (1, 2)
         ################################# Begin your code ###############################
         # Find the minimum-cost control
-        min_cost_control = min(control_cost, key=control_cost.get) # key arg indicates that we want index of min-cost
+        min_cost_control = min(control_cost, key=control_cost.get) # key arg indicates that we want index of min cost
         return min_cost_control
         ################################# End your code ###############################
 
@@ -56,7 +57,7 @@ class standard_rollout:
         # Find cartesian product
         control_combos = self.get_cartesian_product(list_of_all_control_component_sets)
         for combo in control_combos:
-            # Populate list_of_all_control_component_sets with expected sum of stage cost and expected future cost
+            # Populate list_of_all_control_component_sets with expected sum of stage cost and future cost
             control_cost[combo] = self.expectation_for_minimization(taxi_state_object, combo)
         # Find control with minimum expected cost
         min_control = self.minimization_of_expectations(control_cost)
@@ -68,14 +69,15 @@ class one_at_a_time_rollout:
 
     def expectation_for_minimization(self, taxi_state_object, control):
         expected_cost = 0
-        for num_simu in range(10):
+        simulations = 50
+        for num_simu in range(simulations):
             state_object = copy.deepcopy(taxi_state_object) #use state_object for further operations inside this function
             # update expected_cost using the 1) the next_state with the given control 2) future cost using the average_MC_simulation using base policy
             ################################# Begin your code ###############################
             # Add stage cost and expected future cost to expected_cost
             expected_cost += state_object.next_state(control) + average_MC_simulation(state_object)
             ################################# End your code ###############################
-        expected_cost /= 10.0
+        expected_cost /= float(simulations)
         return expected_cost
 
     def minimization_of_expectations(self, control_cost):
@@ -83,7 +85,7 @@ class one_at_a_time_rollout:
         # The function should return 2
         ################################# Begin your code ###############################
         # Find the minimum-cost control
-        min_cost_control = min(control_cost, key=control_cost.get) # key arg indicates that we want index of min-cost
+        min_cost_control = min(control_cost, key=control_cost.get) # key arg indicates that we want index of min cost
         return min_cost_control
         ################################# End your code ###############################
 
@@ -95,17 +97,16 @@ class one_at_a_time_rollout:
         # Populate a dictionary where <key: agent ell's availble control, corresponding stage and future cost using expectation_for_minimization
         # Set the minimizing control component for agent ell in one_at_a_time_rollout_control from the dictionary
         ################################# Begin your code ###############################
-        # Get control given by base policy
-        base_policy_control = base_policy().get_control(taxi_state_object)
         # Create list for all controls
         one_at_a_time_rollout_control = []
         m = taxi_state_object.g.m
         for ell in range(m):
+            # Get control given by base policy
+            base_policy_control = base_policy().get_control(taxi_state_object)
             # Create and populate list of control components
             constructed_control = base_policy_control
-            if ell != 0:
+            if ell > 0:
                 constructed_control[0:ell] = one_at_a_time_rollout_control[0:ell]
-            #constructed_control[ell+1:m] = base_policy_control[ell+1:m]
             control_cost = {}
             # Find available controls for agent ell
             avail_controls = taxi_state_object.available_control_agent(ell)
@@ -143,7 +144,7 @@ def MC_simulation(taxi_state_object, policy_name='base_policy', do_print=True):
             #state_object.print_state()
             None
     if (do_print):
-        print('Inside MC_simulation: Trejectory')
+        print('Inside MC_simulation: Trajectory')
         print(trajectory_string)
         print('trajectory_cost: ',trajectory_cost)
         #print('Inside MC_simulation: trajectory_cost ',trajectory_cost)
