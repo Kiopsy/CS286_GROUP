a121760b7d31201652e4d8e843db3476fa2d49c7
dominicgarrity <dgarrity@college.harvard.edu>
2022-04-20 19:52:40 -0400

    Added another layout.

--- Final_Project/Simulation/simulation.py
+++ Final_Project/Simulation/simulation.py
@@ -5,7 +5,7 @@ from graphics import GraphicsWindow
                 
 def main():
     g = Grid()
-    g.read_Grid('Layouts/layout1_int.txt')
+    g.read_Grid('Layouts/layout3_int.txt')
     # g.plot_grid()
     timestep = 0
     while True:
